4976f59a97d784428de4590e5307d764f125a7ce
iamlothian <lothian.studios@gmail.com>
2014-01-07 19:50:16 +1100

    fixed reference to rucksack.js from rucksack.test.js

--- rucksack.test/rucksack.test.js
+++ rucksack.test/rucksack.test.js
@@ -1,10 +1,4 @@
-﻿/// <reference path="rucksack.js" />
-
-describe("A suite", function () {
-    it("contains spec with an expectation", function () {
-        expect(true).toBe(true);
-    });
-});
+﻿/// <reference path="../rucksack/rucksack.js" />
 
 describe("$rucksack", function () {
     it("exists", function () {
